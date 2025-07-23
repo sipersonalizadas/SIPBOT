54085681a0e2af7a64b345a35bfd770b3f07706e
SIPERSONALIZADAS <SOLUCIONESSIP@OUTLOOK.COM>
2025-07-23 13:29:16 -0500

    Añadir permiso CORS para que el chat funcione

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
